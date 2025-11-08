c3de773bde2755ffb3b601e40f4f55087da44235
ShymeetriaJ <sjeducates2@gmail.com>
2025-11-08 13:52:19 -0500

    Formatted the html.

---
+++ index.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('blogform');
+const newtitile = document.getElementById('newposttitle');
+const textarea = document.getElementById('blogtext');
+const postbtn = document.getElementById('postbtn');
