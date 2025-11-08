34c5afb805376099ee7b8c8c707236a5e4ec04a5
ShymeetriaJ <sjeducates2@gmail.com>
2025-11-08 15:52:06 -0500

    deleted javascript code.

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const form = document.getElementById('blogform');
 const newtitile = document.getElementById('newposttitle');
 const textarea = document.getElementById('blogtext');
 const postbtn = document.getElementById('postbtn');
+
+
+
+
+//the event listener for submitting the form and function for validation
+form.addEventListener('submit', function(event) {
+    event.preventDefault();             //stops the form from submitting to check for v     
+});
+
