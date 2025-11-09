ff1a6a1c305ca51e27869a7a77c419eb077bbbb8
ShymeetriaJ <sjeducates2@gmail.com>
2025-11-08 21:33:55 -0500

    Added showError functions.

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const form = document.getElementById('blogform');
-const newtitile = document.getElementById('newposttitle');
-const textarea = document.getElementById('blogtext');
-const postbtn = document.getElementById('postbtn');
+const titleInput = document.getElementById('newpost-title');
+const textArea = document.getElementById('blogtext');
+const postBtn = document.getElementById('postbtn');
+
+//Declaration for title span 
+const titleError = document.getElementById('titleError');
+
+//Declaration for blog text span
+const textError = document.getElementById('blogtextError');
+
+//Declaration for completed blogs
+const postsList = document.getElementById('completedBlogs')
 
 
+//Functions to showErrors
+function showError(input, message) {
+    const formGroup = input.parentElement;
+    formGroup.classList.add('blog-group-new error');
+    const span = formGroup.querySelector('span');
+    span.innerText = message;
+}
+//clear error input
+function clearError(input) {
+    const formGroup = input.parentElement;
+    formGroup.classList.remove('blog-group-new error');
+    const span = formGroup.querySelector('span');
+    span.innerText = '';
+}
+//function to make post go to list
+function postBlog(title, content) {
+    const li = document.createElement('li');
+    li.className = 'post-item';
+    return li;
+
+// calling the title of the post
+const blogTitle = document.createElement('blogTitle');
+blo
+
+}
 
 
-//the event listener for submitting the form and function for validation
-form.addEventListener('submit', function(event) {
-    event.preventDefault();             //stops the form from submitting to check for v     
-});
 
